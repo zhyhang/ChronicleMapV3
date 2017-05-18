27ad4a5ed7ab9eb01371de2304479b96fb8fa32b
yuhang.zhao <yuhang.zhao@ipinyou.com>
2017-05-18 14:40:46 +0800

    upgrade chronicle3

--- src/main/java/com/zyh/chronicle/map/v3/ChronicleMapRecycleV3.java
+++ src/main/java/com/zyh/chronicle/map/v3/ChronicleMapRecycleV3.java
@@ -28,16 +28,18 @@ public class ChronicleMapRecycleV3 {
 		int maxEntry = 10000;
 		File f = Files.createTempFile("cmap-recycle-test", ".dat").toFile();
 		// create map
+		// exceed max entry will increase to 3.0 * max entry 
 		ChronicleMapBuilder<String, LongValue> mapBuilder = ChronicleMapBuilder.of(String.class, LongValue.class)
-				.entries(maxEntry).averageKeySize(10);
+				.entries(maxEntry).maxBloatFactor(3.0).averageKeySize(10);
 		ChronicleMap<String, LongValue> map = mapBuilder.createPersistedTo(f);
 		for (int i = 0; i < 20 * maxEntry; i++) {
 			LongValue v = THREAD_LOCAL_LONGVALUE.get();
 			v.setValue(i);
 			map.put(String.valueOf(i), v);
-			if (i % 1000 == 0) {
+			if (i % 1000 == 1) {
 				System.out.format("count[%d], max entry[%d],current size[%d]\n", i, maxEntry, map.longSize());
 			}
+			// comment following if, will throw overflow exception
 			if (i >= maxEntry - 1) {
 				while (true) {
 					String removeKey = String.valueOf(ThreadLocalRandom.current().nextInt(i));
@@ -46,6 +48,7 @@ public class ChronicleMapRecycleV3 {
 					}
 				}
 			}
+			
 		}
 		map.close();
 	}
